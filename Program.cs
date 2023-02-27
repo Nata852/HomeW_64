12876aa4d8cfd6e1179b7994c2e6e7818d9e7feb
Natali <natlyly3@mail.ru>
2023-02-27 20:16:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int GetNumber(string message)
+{
+    int resalt = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out resalt))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+    return resalt;
+}
+void WriteNubers(int number)
+{
+    if(number ==1) Console.Write("1 ");
+    else
+    {
+        Console.Write($"{number} ");
+        WriteNubers(number - 1);
+    }
+}
+int num = GetNumber("Введите натуральное число: ");
+WriteNubers(num);
+   
